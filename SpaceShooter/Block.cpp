c8b09d32fa76ddb33811a044dc6097c0c952ef64
YoungKnight337 <warfire9145@gmail.com>
2025-03-06 16:40:58 -0500

    Added Functionality

--- SpaceShooter/Block.cpp
+++ SpaceShooter/Block.cpp
@@ -1,1 +1,26 @@
 #include "Block.h"
+
+Block::Block()
+{
+
+}
+
+Block::~Block()
+{
+
+}
+
+void Block::Draw()
+{
+
+}
+
+void Block::Update()
+{
+
+}
+
+void Block::GetRect()
+{
+	return { width, height, position.x, position.y };
+}
--- SpaceShooter/Block.h
+++ SpaceShooter/Block.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "raylib.h"
 class Block
 {
+private:
+	int width;
+	int height;
+	Vector2 positon;
+
+public:
+	Block();
+	~Block();
+	void Draw();
+	void Update();
+	void GetRect();
 };
 
