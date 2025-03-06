36380888dad84fac86ccf2e5436d24209e90d758
YoungKnight337 <warfire9145@gmail.com>
2025-03-06 16:44:33 -0500

    Added Functionality

--- SpaceShooter/Block.cpp
+++ SpaceShooter/Block.cpp
@@ -20,6 +20,14 @@ void Block::Update()
 
 }
 
+void Block::CheckCollision(Projectile& laser)
+{
+	if(CheckCollisionRecs(GetRect(), laser.GetRect()))
+	{
+	
+	}
+}
+
 void Block::GetRect()
 {
 	return { width, height, position.x, position.y };
--- SpaceShooter/Block.h
+++ SpaceShooter/Block.h
@@ -1,17 +1,23 @@
 #pragma once
 #include "raylib.h"
+#include "Projectile.h";
+
+class Projectile;
+
 class Block
 {
 private:
 	int width;
 	int height;
 	Vector2 positon;
+	Color color;
 
 public:
 	Block();
 	~Block();
 	void Draw();
 	void Update();
+	void CheckCollision(Projectile& laser);
 	void GetRect();
 };
 
