e3b4fc2c11d842bcdc999e4c75becdfaeb9a5817
YoungKnight337 <warfire9145@gmail.com>
2025-03-11 22:44:08 -0400

    Small Changes

--- SpaceShooter/SpaceShooter.cpp
+++ SpaceShooter/SpaceShooter.cpp
@@ -3,16 +3,20 @@
 #include "Alien.h"
 #include "AlienManager.h"
 #include "Projectile.h"
+#include "BlockManager.h"
 
 int main()
 {
     constexpr int ALIEN_ROW = 5;
     constexpr int ALIEN_COL = 11;
 
+    constexpr int BLOCK_ROW = 4;
+
     constexpr int SCREEN_WIDTH = 750;
     constexpr int SCREEN_HEIGHT = 700;
     constexpr int FRAME_RATE = 60;
 
+   
     
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
@@ -20,17 +24,18 @@ int main()
 
     Player player;
     //Alien alien;
-    //AlienManager spawner;
+    AlienManager spawner;
     Projectile laser(5,5,5.0f,false,GREEN);
-    //Projectile beam(5, 5, 5.0f, true, RED);
+    Projectile beam(5, 5, 5.0f, true, RED);
+    BlockManager row();
 
     //spawner.Initialize(alien, ALIEN_ROW, ALIEN_COL);
 
     while (WindowShouldClose()==false)
     {
         //Update
-        player.Update(laser);
-        //spawner.Update();
+        player.Update(beam);
+        spawner.Update(beam, ALIEN_ROW, ALIEN_COL);
 
 
         BeginDrawing();
