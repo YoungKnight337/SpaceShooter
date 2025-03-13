6b445ec125cef81ba887534cf18e8abe5ba52d5e
YoungKnight337 <warfire9145@gmail.com>
2025-03-12 22:29:56 -0400

    Simple Change

--- SpaceShooter/AlienManager.cpp
+++ SpaceShooter/AlienManager.cpp
@@ -58,10 +58,15 @@ void AlienManager::Update(Projectile& laser)
 
 void AlienManager::MoveAliens()
 {
-	//Moves Collection of Aliens
 	//Control
 	//Moves down
+	// for(int i = 0; i < alien.size(); i++)
+	// {
 	//	position.x += 5;
-	//Moves down
+	//	if (position.x >= GetScreenWidth())
+	//		position.y +=5;
 	//	position.x -= 5;
+	//	if (position.x <= 0)
+	//		position.y -= 5;
+	// }
 }
