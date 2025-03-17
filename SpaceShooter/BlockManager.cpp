c8a1d402bba3c5ca4dac10bb5e0a529976f40755
YoungKnight337 <warfire9145@gmail.com>
2025-03-16 23:09:25 -0400

    Adjusted Positioning

--- SpaceShooter/BlockManager.cpp
+++ SpaceShooter/BlockManager.cpp
@@ -21,6 +21,7 @@ void BlockManager::Initialize(Block& block, int a)
 		std::vector<Block> blocks;
 
 		//block.position.y = i * (block.GetHeight() + 10) + i;
+		//block.position.x = ;
 		
 		blocks.push_back(block);
 	}
