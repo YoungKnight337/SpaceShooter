6feac81442a402af5b4055c30dc4ee40eb431e91
YoungKnight337 <warfire9145@gmail.com>
2025-03-22 23:24:02 -0400

    Small Change

--- SpaceShooter/BlockManager.cpp
+++ SpaceShooter/BlockManager.cpp
@@ -15,7 +15,7 @@ void BlockManager::Initialize(Block& block, int a)
 {
 	for (int i = 0; i < a; i++)
 	{
-		block.position.y = (block.GetHeight() - 10);
+		block.position.y = (block.GetHeight() + 10) ;
 		block.position.x = i * (block.GetWidth() + 1);
 		
 		blocks.push_back(block);
