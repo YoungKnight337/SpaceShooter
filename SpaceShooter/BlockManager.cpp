9d45d9b24c54e44aa3aba28350cf45db9814753a
YoungKnight337 <warfire9145@gmail.com>
2025-03-22 21:45:02 -0400

    Changed Logic For Block Manager

--- SpaceShooter/BlockManager.cpp
+++ SpaceShooter/BlockManager.cpp
@@ -13,9 +13,9 @@ BlockManager::~BlockManager()
 //Need to fix Blocks need to be initialized in a a typical array/vector positioning vertically is all the same but the horizontal positioning is seperate.
 void BlockManager::Initialize(Block& block, int a)
 {
-	for (int i = 0; i > a; i++)
+	for (int i = 0; i < a; i++)
 	{
-		position.y = i * (block.GetHeight() + 10) + i;
+		block.position.y = (block.GetHeight() + 10) + i;
 		block.position.x = (block.GetWidth() + 1);
 		
 		blocks.push_back(block);
