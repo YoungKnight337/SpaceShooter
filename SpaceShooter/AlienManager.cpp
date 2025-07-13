9e280927e9dcbb89664bf2db99ed8a6fc3a9e065
YoungKnight337 <warfire9145@gmail.com>
2025-07-12 22:19:53 -0400

    Small Change

--- SpaceShooter/AlienManager.cpp
+++ SpaceShooter/AlienManager.cpp
@@ -28,7 +28,7 @@ void AlienManager::Initialize(Alien& alien, int a, int b)
 		{
 			if (i == 0)
 				Alien octopus(true, type::OCTOPUS);
-				position.x = j * (alien .GetWidth() + 10) + j;
+				position.x = j * (alien.GetWidth() + 10) + j;
 				position.y = i * (alien.GetHeight() + 10) + i;
 			if (i == 1 || i == 2)
 				Alien squid (true, type::SQUID);
@@ -36,7 +36,7 @@ void AlienManager::Initialize(Alien& alien, int a, int b)
 				position.y = i * (alien.GetHeight() + 10) + i;
 			if (i == 3)
 				Alien crab (true, type::CRAB);
-				position.x = j * (alien.GetWidth() + 10) + j ;
+				position.x = j * (alien.GetWidth() + 10) + j;
 				position.y = i * (alien.GetHeight() + 10) + i;
 			
 			alien_row.push_back(alien);
