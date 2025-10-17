ffa6d4aa2197186ac40a079b0e0fbe182cfea037
YoungKnight337 <warfire9145@gmail.com>
2025-10-16 21:53:09 -0400

    Small Change

--- SpaceShooter/BlockManager.h
+++ SpaceShooter/BlockManager.h
@@ -18,7 +18,7 @@ public:
 	void Update(Player& player);
 	void CheckCollision(Player& player);
 	void DeleteBlocks();
-	Block blocks[BLOCK_ROW];
+	Block* blocks = new Block[BLOCK_ROW];
 	std::vector<Block> blocks;
 };
 
